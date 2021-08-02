27f7640f6d0d33d837970d60c3c6e94d208ff082
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-02 13:37:33 +0200

    Added a 7 days forecast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "regenerator-runtime/runtime";
+import { fromUnixTime, format } from "date-fns";
 import fitText from "./fit-text.js";
 import Weather from "./weather.js";
 
@@ -6,7 +7,7 @@ const Getter = (() =>
 {
 	const GetLocation = () => new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
 
-	const GetWeather = async () =>
+	const GetCurrentWeather = async () =>
 	{
 		let userLocation;
 		try
@@ -16,23 +17,36 @@ const Getter = (() =>
 		{
 			if (err.message === "User denied Geolocation") return alert("Whatever, cunt");
 		}
-		return Weather.LocationCurrentInfo(userLocation);
+		return Weather.LocationCurrentInfoCoords(userLocation);
 	};
 
-	return { GetWeather };
+	const GetForecast = async () =>
+	{
+		let userLocation;
+		try
+		{
+			userLocation = await GetLocation();
+		} catch (err)
+		{
+			if (err.message === "User denied Geolocation") return alert("Whatever, cunt");
+		}
+		return Weather.LocationForecastInfoCoords(userLocation);
+	};
+
+	return { GetCurrentWeather, GetForecast };
 })();
 
 const Interface = (() =>
 {
-	const city = document.querySelector("#city");
-	const currentIcon = document.querySelector("#current-temp .weather-icon img");
-	const currentTemp = document.querySelector("#current-temp .temp");
-	const currentWind = document.querySelector("#current-wind .wind");
-	const currentHumidity = document.querySelector("#current-humidity .humidity");
-
 	const DisplayCurrentWeather = async () =>
 	{
-		const currentWeather = await Getter.GetWeather();
+		const city = document.querySelector("#city");
+		const currentIcon = document.querySelector("#current-temp .weather-icon img");
+		const currentTemp = document.querySelector("#current-temp .temp");
+		const currentWind = document.querySelector("#current-wind .wind");
+		const currentHumidity = document.querySelector("#current-humidity .humidity");
+	
+		const currentWeather = await Getter.GetCurrentWeather();
 		console.log(currentWeather);
 
 		city.textContent = currentWeather.name;
@@ -45,8 +59,31 @@ const Interface = (() =>
 
 		currentHumidity.textContent = currentWeather.main.humidity;
 	};
+
+	const DisplayForecast = async () =>
+	{
+		const daysDOM = document.querySelectorAll(".day-column");
+		const forecast = await Getter.GetForecast();
+		console.log(forecast);
+
+		for (let i = 0; i < daysDOM.length; i++)
+		{
+			let dayName = daysDOM[i].querySelector(".day");
+			if (dayName.id === "d0") dayName.textContent = "Today";
+			else dayName.textContent = format(fromUnixTime(forecast.daily[i].dt), "EEEE");
+			fitText(5, dayName.parentElement.parentElement, dayName);
+
+			daysDOM[i].querySelector(".day-status").textContent = forecast.daily[i].weather[0].main;
+
+			daysDOM[i].querySelector("img").src = `./weather-icons/${forecast.daily[i].weather[0].icon}.png`;
+
+			daysDOM[i].querySelector(".min").textContent = Math.round(forecast.daily[i].temp.min);
+			daysDOM[i].querySelector(".max").textContent = Math.round(forecast.daily[i].temp.max);
+		}
+	};
 	
-	return { DisplayCurrentWeather };
+	return { DisplayCurrentWeather, DisplayForecast };
 })();
 
 Interface.DisplayCurrentWeather();
+Interface.DisplayForecast();
--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 const Weather = (() =>
 {
-	const LocationCurrentInfo = async _location =>
+	const LocationCurrentInfoCoords = async _location =>
 	{
 		return await new Promise((resolve, reject) =>
 		{
@@ -9,7 +9,15 @@ const Weather = (() =>
 		});
 	};
 
-	return { LocationCurrentInfo };
+	const LocationForecastInfoCoords = async _location =>
+	{
+		return await new Promise((resolve, reject) =>
+		{
+			fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${_location.coords.latitude}&lon=${_location.coords.longitude}&exclude=current,minutely,hourly,alerts&units=metric&appid=f49e171463b5cd51a79e76a3a37d883e`, {mode: "cors"})
+				.then(response => response.json()).then(resolve, reject);
+		});	};
+
+	return { LocationCurrentInfoCoords, LocationForecastInfoCoords };
 })();
 
 // 7 Days forecast
