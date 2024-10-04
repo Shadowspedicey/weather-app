cc54fcdff7a48d7f6f522de3cdbfff59e53dd384
Ahmed Khaled <shadowspedicey@gmail.com>
2024-10-04 11:28:02 +0300

    Added a location manager to get current location coords and city name

---
+++ scripts/locationManager.js
@@ -0,0 +1,17 @@
+const locationManager = (() => {
+	const _info = {};
+	async function getCurrentPosition() {
+		navigator.geolocation.getCurrentPosition(async pos => {
+			_info.lat = pos.coords.latitude;
+			_info.long = pos.coords.longitude;
+			const reverseGeo = await fetch(`https://us1.locationiq.com/v1/reverse?key=pk.428cd04206da9290f0e6d75425cc061c&lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&format=json&accept-language=en`);
+			const reverseGeoJson = await reverseGeo.json();
+			_info.city = reverseGeoJson.address.city;
+		})
+
+	}
+	
+	return {getCurrentPosition, get info() {return _info;}}
+})();
+
+export default locationManager;
