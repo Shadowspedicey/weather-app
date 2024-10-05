a5c3bf332e06cd98171fcf7ce6f2ec9166b53b49
Ahmed Khaled <shadowspedicey@gmail.com>
2024-10-05 07:41:31 +0300

    Trying to fix pics not loading

--- scripts/domManager.js
+++ scripts/domManager.js
@@ -50,7 +50,7 @@ const domManager = (() => {
 		weekForecastEl.querySelectorAll(".day").forEach((e, i) => {
 			const dayName = i == 0 ? "Today" : helper.getDayName(helper.addDays(new Date(), i));
 			e.querySelector("h4").textContent = dayName;
-			e.querySelector("img").src = `../icons/${info.days[i].icon}.svg`;
+			e.querySelector("img").src = `/weather-app/icons/${info.days[i].icon}.svg`;
 			e.querySelector("p").textContent = info.days[i].temp;
 		});
 	}
